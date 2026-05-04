0ada382530d70ede3e570ad4b45f92d238b8caa1
Luca <lucaisdik7@gmail.com>
2026-05-04 15:27:40 +0200

    yes

---
+++ src/main/java/me/lucanius/timer/Timer.java
@@ -0,0 +1,16 @@
+package me.lucanius.timer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Timer extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
